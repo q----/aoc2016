51e56807ec439bc1b33d6e180220e615ea63f982
q <root@gitlab.local>
2021-04-18 18:59:02 -0500

    day 2 part 2

--- aoc02/c.cpp
+++ aoc02/c.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 #include<fstream>
-#include<sstream>
 
 using namespace std;
 
@@ -9,8 +8,35 @@ struct key{
   int y = 1;
 };
 
+struct key2{
+  int x = -2;
+  int y = 0;
+};
+
+int f(int x){
+  if(x < 0) return -x;
+  return x;
+}
+
+int operator-(const key2& a){
+  return f(a.x) + f(a.y);
+}
+
 string operator!(const key& a){
   return to_string(1 + 3 * a.x + a.y);
+}
+
+string operator!(const key2& a){
+  string d = "0123456789ABCD";
+  int x;
+  if(f(a.y) == 2){
+    x = (a.y > 0)?13:1;
+  } else if (f(a.y) == 1){
+    x = a.x + ((a.y > 0)?11:3);
+  } else {
+    x = a.x + 7;
+  }
+  return d.substr(x,1);
 }
 
 void operator+=(key& a, const char& b){
@@ -25,7 +51,24 @@ void operator+=(key& a, const char& b){
   }
 }
 
-string p(key& a, string l){
+void operator+=(key2& a, const char& b){
+  if(b == 'U'){
+    a.y--;
+    if(-a > 2) a.y++;
+  } else if (b == 'D'){
+    a.y++;
+    if(-a > 2) a.y--;
+  } else if (b == 'L'){
+    a.x--;
+    if(-a > 2) a.x++;
+  } else {
+    a.x++;
+    if(-a > 2) a.x--;
+  }
+}
+
+template <class T>
+string p(T& a, string l){
   for(char c : l) a += c;
   return !a;
 }
@@ -34,11 +77,17 @@ int main(){
   ifstream f("input");
   string l;
   string o;
+  string o2;
 
   key a;
-  while(getline(f,l)) o += p(a,l);
+  key2 b;
+  while(getline(f,l)){
+    o += p(a,l);
+    o2 += p(b,l);
+  }
 
   cout << o << endl;
+  cout << o2 << endl;
 
   return 0;
 }
